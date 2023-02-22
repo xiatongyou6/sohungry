a48e27a251806f8380d3a07ce033718ac7e8466a
‘xiatongyou6’ <xiatongyou6@outlook.com>
2023-02-22 20:53:52 +0800

    go practice

---
+++ lang/go/go_projects/umRpc/go.mod
@@ -0,0 +1,3 @@
+module umRpc
+
+go 1.19
---
+++ lang/go/go_projects/umRpc/main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"time"
+	umrpc "umRpc"
+	"umRpc/codec"
+)
+
+func startServer(addr chan string) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	umrpc.Accept(l)
+}
+
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	conn, _ := net.Dial("tcp", <-addr)
+	defer func() {
+		_ = conn.Close()
+	}()
+	time.Sleep(time.Second)
+	_ = json.NewEncoder(conn).Encode(umrpc.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+	for i := 0; i < 5; i++ {
+		h := &codec.Header{
+			ServiceMethod: "Foo.Sum",
+			Seq:           uint64(i),
+		}
+		_ = cc.Write(h, fmt.Sprintf("umrpc req %d", h.Seq))
+		_ = cc.ReadHeader(h)
+		var reply string
+		_ = cc.ReadBody(&reply)
+		log.Println("reply:", reply)
+	}
+}
